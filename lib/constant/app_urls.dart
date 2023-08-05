fcd0060db584785155c144f157c55261a05288b9
Aiko <aikocute@icloud.com>
2023-08-05 12:49:01 +0700

    Update

--- lib/constant/app_urls.dart
+++ lib/constant/app_urls.dart
@@ -1,12 +1,13 @@
 class AppUrls {
-  static const String baseUrl = 'https://arclightnetwork.xyz'; // 基础接口地址
-  static const String baseApiUrl = '$baseUrl/api/v1'; // 基础接口地址
+  static const String baseUrl = 'https://aikocute.tech'; 
+  static const String subscribeUrl = 'https://aikocute.click';
+  static const String baseApiUrl = '$baseUrl/api/v1';
 
-  static const String login = '$baseApiUrl/passport/auth/login';
-  static const String register = '$baseApiUrl/passport/auth/register';
+  static const String login = '$baseApiUrl/passport/aiko/login';
+  static const String register = '$baseApiUrl/passport/aiko/register';
   static const String getQuickLoginUrl = '$baseApiUrl/passport/auth/getQuickLoginUrl';
 
-  static const String userSubscribe = '$baseApiUrl/user/getSubscribe';
+  static const String userSubscribe = '$subscribeUrl/user/getSubscribe';
   static const String plan = '$baseApiUrl/guest/plan/fetch';
   static const String server = '$baseApiUrl/user/server/fetch';
   static const String userInfo = '$baseApiUrl/user/info';
